d0b2be3f5f54cc231e30f581ed67190f92866bf0
DamianRoc13 <146010485+DamianRoc13@users.noreply.github.com>
2024-05-14 07:13:55 -0500

    1

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+  } from 'class-validator';
+  
+  // create-user-dto
+  export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    fullName: string;
+  
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+  }
+  
